12b396122cd2eaf31fb55a71d14f71a501e6416a
MohammadAli Arjomand <arjomand.dev@gmail.com>
2025-09-01 16:26:26 +0330

    show quality component in the modal

--- components/EpisodesList.tsx
+++ components/EpisodesList.tsx
@@ -1,16 +1,28 @@
 import { loadEpisodes } from "@/services/load-movie-data";
 import { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Modal, Portal } from "react-native-paper";
+import QualitiesList from "./QualitiesList";
 
 export default function EpisodesList({title, season}: {title: string, season: string}) {
     const [episodes, setEpisodes] = useState<{episode: string}[]>([]);
     loadEpisodes(title, season).then(episodes => setEpisodes(episodes || []));
+
+    const [visible, setVisible] = useState(false);
+
     return (
         <View>
             {episodes.map(episode => (
-                <TouchableOpacity key={episode.episode} style={{ padding: 8, borderBottomWidth: 1, borderBottomColor: "#eee" }}>
-                    <Text style={styles.listItem}>Episode {episode.episode}</Text>
-                </TouchableOpacity>
+                <View>
+                    <TouchableOpacity onPress={() => setVisible(true)} key={episode.episode} style={{ padding: 8, borderBottomWidth: 1, borderBottomColor: "#eee" }}>
+                       <Text style={styles.listItem}>Episode {episode.episode}</Text>
+                    </TouchableOpacity>
+                    <Portal>
+                        <Modal visible={visible} onDismiss={() => setVisible(false)} contentContainerStyle={{backgroundColor: 'white', padding: 20, margin: 20, borderRadius: 8}}>
+                            <QualitiesList/>
+                        </Modal>
+                    </Portal>
+                </View>
             ))}
         </View>
     )
