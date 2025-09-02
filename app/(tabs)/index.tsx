6a15a7d46631f202e958849f6e1fa7586561c9b0
MohammadAli Arjomand <arjomand.dev@gmail.com>
2025-09-03 00:15:02 +0330

    complete home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,20 @@
+import { openURL } from "expo-linking";
 import { useRouter } from "expo-router";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TouchableOpacity } from "react-native";
 export default function Home() {
     const router = useRouter();
     return (
-        <View>
-            <Text style={styles.header}>Open Movie</Text>
+        <ScrollView>
+            <Text style={styles.header}>Welcome to Open Movie!</Text>
 
-            <TouchableOpacity onPress={() => router.push("/movie/Breaking Bad")}>
-                <Text>Open Breaking Bad</Text>
+            <Text style={styles.body}>Download full database from our Telegram channel and import it!</Text>
+            <TouchableOpacity onPress={() => openURL("https://t.me/OpenMovieApp")}>
+                <Text style={styles.link}>- Telegram Channel?</Text>
             </TouchableOpacity>
-        </View>
+            <TouchableOpacity onPress={() => router.push("/settings/databases")}>
+                <Text style={styles.link}>- How to import?</Text>
+            </TouchableOpacity>
+        </ScrollView>
     )
 }
 
@@ -20,4 +25,14 @@ const styles = StyleSheet.create({
         textAlign: "center",
         marginTop: 20,
     },
+    body: {
+        padding: 8,
+        fontSize: 16   
+    },
+    link: {
+        padding: 8,
+        fontSize: 16,
+        paddingVertical: 12,
+        color: '#007BFF'
+    }
 })
