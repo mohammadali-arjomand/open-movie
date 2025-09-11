20672dbba8637239fbf49473fd5d6c0944360246
MohammadAli Arjomand <arjomand.dev@gmail.com>
2025-09-11 17:48:54 +0330

    removed unused prop

--- app/(tabs)/bookmarks.tsx
+++ app/(tabs)/bookmarks.tsx
@@ -25,7 +25,6 @@ export default function Bookmarks() {
                     renderItem={({item}) => (
                         <MovieCard title={item} />
                     )}
-                    contentContainerStyle={{}}
                 />
             </View>
         )
