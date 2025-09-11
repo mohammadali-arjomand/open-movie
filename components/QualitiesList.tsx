4d1d005a2725019d01c396fbd7f2af0db4a6c3c0
MohammadAli Arjomand <arjomand.dev@gmail.com>
2025-09-11 18:10:22 +0330

    fix indent

--- components/QualitiesList.tsx
+++ components/QualitiesList.tsx
@@ -7,75 +7,76 @@ import { useEffect, useState } from "react";
 import { Alert, Linking, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function QualitiesList({title, season, episode, setSelectedItem}: {title: string, season: string, episode: string, setSelectedItem: any}) {
-  const router = useRouter()
+    const router = useRouter()
 
-  const [copiedItem, setCopiedItem] = useState<string>("")
-  const [qualities, setQualities] = useState<{quality: string, language: string, url: string}[]>([]);
-  useEffect(() => {
-    loadQualities(title, season, episode).then(qualities => setQualities(qualities || []));
-  }, [])
-  
-  const openUrl = (url: string) => {
-    setSelectedItem('')
-    Linking.canOpenURL(`vlc://${url}`)
-      .then((supported) => {
-        if (supported) {
-          Linking.openURL(`vlc://${url}`)
-        }
-        else {
-          Alert.alert(
-            "VLC is Required", 
-            "VLC Player is not installed. Please install VLC to play this video.",
-            [
-              {
-                text: "Cancel",
-                style: "cancel"
-              },
-              {
-                text: "Install",
-                style: "default",
-                onPress: () => {
-                  Linking.openURL("https://play.google.com/store/apps/details?id=org.videolan.vlc")
+    const [copiedItem, setCopiedItem] = useState<string>("")
+    const [qualities, setQualities] = useState<{quality: string, language: string, url: string}[]>([]);
+    useEffect(() => {
+        loadQualities(title, season, episode).then(qualities => setQualities(qualities || []));
+    }, [])
+    
+    const openUrl = (url: string) => {
+        
+        setSelectedItem('')
+        Linking.canOpenURL(`vlc://${url}`)
+        .then((supported) => {
+            if (supported) {
+            Linking.openURL(`vlc://${url}`)
+            }
+            else {
+            Alert.alert(
+                "VLC is Required", 
+                "VLC Player is not installed. Please install VLC to play this video.",
+                [
+                {
+                    text: "Cancel",
+                    style: "cancel"
+                },
+                {
+                    text: "Install",
+                    style: "default",
+                    onPress: () => {
+                    Linking.openURL("https://play.google.com/store/apps/details?id=org.videolan.vlc")
+                    }
                 }
-              }
-            ]
-          )
-        }
-      })
-      .catch((err) => {
-        console.error("> " + err)
-      })
-  }
+                ]
+            )
+            }
+        })
+        .catch((err) => {
+            console.error("> " + err)
+        })
+    }
 
-    const styles = StyleSheet.create({
-        listItem: {
-            fontSize: 16,
-            borderBottomWidth: 1,
-            borderBottomColor: useThemeColor("border"),
-            color: useThemeColor("text"),
-            paddingVertical: 16,
-            paddingHorizontal: 6,
-            flex: 1,
-            justifyContent: 'space-between',
-            flexDirection: 'row',
-            alignItems: 'center'
-        }
-    })
+        const styles = StyleSheet.create({
+            listItem: {
+                fontSize: 16,
+                borderBottomWidth: 1,
+                borderBottomColor: useThemeColor("border"),
+                color: useThemeColor("text"),
+                paddingVertical: 16,
+                paddingHorizontal: 6,
+                flex: 1,
+                justifyContent: 'space-between',
+                flexDirection: 'row',
+                alignItems: 'center'
+            }
+        })
 
-    const copyUrl = (url: string) => setStringAsync(url).then(() => setCopiedItem(url))
+        const copyUrl = (url: string) => setStringAsync(url).then(() => setCopiedItem(url))
 
-  return (
-    <View>
-        {qualities.length > 0 ? qualities.map(quality => (
-            <TouchableOpacity style={styles.listItem} key={quality.url} onPress={() => openUrl(quality.url)}>
-                <Text>
-                    {quality.quality} ({quality.language == 'sub' ? 'Subtitle' : (quality.language == 'dub' ? 'Dubbed' : 'Trailer')})
-                </Text>
-                <TouchableOpacity onPress={() => copyUrl(quality.url)} style={{borderColor: styles.listItem.borderBottomColor, borderWidth:0.5, borderRadius: 8, padding: 5}}>
-                    <Ionicons name={copiedItem === quality.url ? "copy" : "copy-outline"} size={20}/>
+    return (
+        <View>
+            {qualities.length > 0 ? qualities.map(quality => (
+                <TouchableOpacity style={styles.listItem} key={quality.url} onPress={() => openUrl(quality.url)}>
+                    <Text>
+                        {quality.quality} ({quality.language == 'sub' ? 'Subtitle' : (quality.language == 'dub' ? 'Dubbed' : 'Trailer')})
+                    </Text>
+                    <TouchableOpacity onPress={() => copyUrl(quality.url)} style={{borderColor: styles.listItem.borderBottomColor, borderWidth:0.5, borderRadius: 8, padding: 5}}>
+                        <Ionicons name={copiedItem === quality.url ? "copy" : "copy-outline"} size={20}/>
+                    </TouchableOpacity>
                 </TouchableOpacity>
-            </TouchableOpacity>
-        )) : <Text>Loading...</Text>}
-    </View>
-  );
+            )) : <Text>Loading...</Text>}
+        </View>
+    );
 }
